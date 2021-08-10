d5b3b87641fa7ecd8ec27339648676386ca0a314
Jared Lewis <jareddlewis@gmail.com>
2021-08-10 12:44:03 -0500

    sconnected the server to the front end with webpack

---
+++ client/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Math Game</h1>
+        <p>Hello world. I made it</p>
+      </div>
+    )
+  }
+}
+
+export default App;
